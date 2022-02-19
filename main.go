661eccdc02c136454b8c22c1a1e87db398f77a31
OneOfTheAleks <99807823+OneOfTheAleks@users.noreply.github.com>
2022-02-19 12:24:57 +0300

    Update main.go

--- main.go
+++ main.go
@@ -3,23 +3,43 @@ package main
 import (
 	"GoTogether/apiServer"
 	"go.uber.org/zap"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
 )
 
 var configPath string
 
 func init(){
-
+configPath = "configs/apiServer.yaml"
 }
 
 func main() {
+
+
 
 	logger,_ := zap.NewProduction()
 	defer logger.Sync()
 
 	config := apiServer.NewConfig()
+ //   C:=&config
+	yamlFile, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		log.Printf("yamlFile.Get err   #%v ", err)
+	}
+
+	err = yaml.Unmarshal(yamlFile,config)//C
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+
+
+
+
+
 	s := apiServer.New(config)
 	if err := s.Start(); err != nil {
-		logger.Info("Can't run server")
+		logger.Error("Can't run server")
 		//log.Fatal(err)
 	}
 }
