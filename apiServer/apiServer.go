d0deacd939d09e0141e131e30441d26de4c744b9
OneOfTheAleks <99807823+OneOfTheAleks@users.noreply.github.com>
2022-02-19 12:24:26 +0300

    Update apiServer.go

--- apiServer/apiServer.go
+++ apiServer/apiServer.go
@@ -12,28 +12,27 @@ type ApiServer struct {
 	router *mux.Router
 }
 
-func New (config *Config) *ApiServer {
+func New(config *Config) *ApiServer {
 	return &ApiServer{
 		config: config,
-	//	logger: zap.Logger{}
-	    router: mux.NewRouter(),
+		//	logger: zap.Logger{}
+		router: mux.NewRouter(),
 	}
 }
 
 func (s *ApiServer) Start() error {
 
-s.ConfigureRouter()
+	s.ConfigureRouter()
 
-
-return http.ListenAndServe(s.config.BindAdrr,s.router)
+	return http.ListenAndServe(s.config.BindAdrr, s.router)
 }
 
-func (s *ApiServer) ConfigureRouter()  {
-	s.router.HandleFunc("/hello",s.HandleHallo())
+func (s *ApiServer) ConfigureRouter() {
+	s.router.HandleFunc("/hello", s.HandleHallo())
 }
 
-func (s * ApiServer) HandleHallo() http.HandlerFunc  {
-  return func(w http.ResponseWriter, r *http.Request) {
-	 io.WriteString(w,"Hello")
-  }
+func (s *ApiServer) HandleHallo() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello")
+	}
 }
