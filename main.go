15f24a985246459b5f3d127ba0ea6c32e11d5256
OneOfTheAleks <99807823+OneOfTheAleks@users.noreply.github.com>
2022-05-25 22:42:56 +0300

    Update main.go

--- main.go
+++ main.go
@@ -3,19 +3,17 @@ package main
 import (
 	"GoTogether/apiServer"
 	"fmt"
-	_ "github.com/jackc/pgx/v4"
-	"go.uber.org/zap"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
 )
 
 var configPath string
-//var logPath    string
+var LogPath    string
 
 func init() {
 	configPath = "configs/apiServer.yaml"
-//	logPath     = "logs/log.json"
+	LogPath     = "logs/log.json"
 //	createLog(logPath)
 }
 
@@ -41,33 +39,17 @@ func main() {
 	}
 
 
-	s := apiServer.New(config)
-	if err := s.Start(); err != nil {
-	//	apiLogger.Error(
-	//		"Can't run server",
-	//		zap.String("apiServer", "Run"),
-	//		zap.Error(err))
 
-		//	addServerLog(config.Logger.,err)
+	if err := apiServer.Start(config); err != nil {
+
 		fmt.Println(err)
+	}
+	if err == nil  {
+		fmt.Println("Server running")
 	}
 }
 
 //---------------------------------
 
-func addLog(log *zap.Logger, msg string, k string, v string, err error) {
-	log.Error(
-		msg,
-		zap.String(k, v),
-		zap.Error(err))
-}
 
-func addServerLog(log *zap.Logger,err error)  {
-	errString := "Api server error"
-	log.Error(
-		errString,
-		zap.Error(err),
-		)
-
-}
 
